010968fb3911145eabfdddb67ab704a6668c8a93
Diego Rocha <diegosr.udi@gmail.com>
2026-01-31 09:32:00 -0300

    iniciando meu projeto

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Agora é React!</h1>
+      <p>Bora pra cima Diego!</p>
+    </div>
+  );
+}
+
+export default App;
